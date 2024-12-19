c59f75aaf12283bb78a8db3c620f26704ee46105
Deepesh Dandriyal <deepeshdandriyal1@gmail.com>
2024-12-19 13:28:07 +0530

    add shadcn and clerk

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Namastey guyss</div>;
+  return (
+    <div>
+      <Button variant="destructive">Namastey</Button>
+    </div>
+  );
 }
